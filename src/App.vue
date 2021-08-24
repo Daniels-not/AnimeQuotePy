ecaf9020d76981a03f29d6e990fe744c41699106
Daniels-not <ramydcampusano@hotmail.com>
2021-08-23 20:07:40 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <AppHeader title="Anime Quotes Py"/>
+    <Quote :quote="qoute"/>
+    <div class="btn-container">
+      <button @click="loadQuotes">Generate Qoute</button>
+    </div>
+    <QuoteHistory :quote="qoutes" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from './components/Header.vue'
+import Quote from './components/Quote.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+    Quote,
+  },
+  data () {
+    return {
+      qoute: {},
+      qoutes: []
+    }
+  },
+  methods: {
+    async loadQuotes () {
+      const data = await fetch("https://animechan.vercel.app/api/random").then(res => res.json());
+
+      this.qoute = {
+        content: data.quote,
+        anime: data.anime,
+        characters: data.character
+      }
+    }
+  },
+
+  created () {
+    this.loadQuotes();
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./sass/main.scss";
+
+.btn-container{
+  display: flex;
+  justify-content: center;
+  padding: 0px 2rem;
+  margin: 4rem auto;
+
+  button{
+    border: none;
+    outline: none;
+    background-color: get_color(primary);
+
+    padding: 1rem 2rem;
+    border-radius: 6rem;
+
+    color: get_color(light);
+    font-size: 1.5rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 0.1rem;
+    cursor: pointer;
+    transition: 0.5s;
+
+    &:hover{
+      background-color: get_color(secondary);
+    }
+  }
 }
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+      <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['title'],
+}
+</script>
+
+<style lang="scss">
+@import "../sass/main.scss";
+
+header {
+    display: flex;
+    justify-content: center;
+    @include toRem(padding-top, 24);
+
+    h1{
+        @include toRem(font-size, 32);
+        color: get_color(dark);
+        font-weight: 700;
+        text-align: center;
+        text-transform: uppercase;
+        @include toRem(padding-bottom, 8);
+        border-bottom: 0.32rem solid get_color(primary);
+    }
+}
+</style>
